23c24d0a96a9e14cf7fa1e014bf4d71928b2dfc1
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2020-06-23 02:53:04 +0000

    add comment model and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,10 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
-    validates :description, presence: true, length: { minimum: 5, maximum: 500 } 
+    validates :description, presence: true, length: { minimum: 5, maximum: 650 } 
     belongs_to :chef
     validates :chef_id, presence: true
     default_scope -> {order(updated_at: :desc)}
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+    has_many :comments, dependent: :destroy
 end
