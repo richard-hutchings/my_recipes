49ddd5abbd36561870515e2b1a2ff4bdab67be51
Ubuntu <ubuntu@ip-172-31-31-120.us-east-2.compute.internal>
2020-06-25 02:38:45 +0000

    make app production ready with action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://greatrecipeapp.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://greatrecipeapp.herokuapp.com', 'http://greatrecipeapp.herokuapp.com']
 end
